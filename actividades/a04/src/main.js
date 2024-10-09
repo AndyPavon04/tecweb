c5479fd341b080c0f1f578cfb754ec873e03ac9b
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-10-08 18:38:01 -0600

    a04: Realizacion ejercicios 4-6 de main.js

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -41,3 +41,41 @@ function ejemplo3(){
     document.write(edad);
     document.write(' años');
 }
+
+function ejemplo4(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);
+}
+
+function ejemplo5(){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>5) {
+        document.write(nombre+' esta aprobado con un '+nota);
+    }
+}
+
+function ejemplo6(){
+    var num1,num2;
+    num1 = prompt('Ingresa el primer número:', '');
+    num2 = prompt('Ingresa el segundo número:', '');
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    if (num1>num2) {
+        document.write('El número mayor es '+num1);
+    }
+    else {
+        document.write('El número mayor es '+num2);
+    }
+}
