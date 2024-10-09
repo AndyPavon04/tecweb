f8cb7439c629910afdc02ebf0c332f5f41aa8c4b
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-10-08 18:41:29 -0600

    a04: Realizacion ejercicios 14-16 de main.js

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -201,3 +201,37 @@ function ejemplo13(){
         document.write(f+"  ");
     }
 }
+
+function ejemplo14(){
+    document.write("Cuidado<br>");
+    document.write("Ingresa tu documento correctamente<br>");
+    document.write("Cuidado<br>");
+    document.write("Ingresa tu documento correctamente<br>");
+    document.write("Cuidado<br>");
+    document.write("Ingresa tu documento correctamente<br>");
+}
+
+function ejemplo15(){
+    function mostrarMensaje() {
+        document.write("Cuidado<br>");
+        document.write("Ingresa tu documento correctamente<br>");
+    }
+    mostrarMensaje();
+    mostrarMensaje();
+    mostrarMensaje();
+}
+
+function ejemplo16(){
+    function mostrarRango(x1,x2) {
+        var inicio;
+        for(inicio=x1; inicio<=x2; inicio++) {
+            document.write(inicio+' ');
+        }
+    }
+    var valor1,valor2;
+    valor1 = prompt('Ingresa el valor inferior:', '');
+    valor1 = parseInt(valor1);
+    valor2 = prompt('Ingresa el valor superior:', '');
+    valor2 = parseInt(valor2);
+    mostrarRango(valor1,valor2);
+}
