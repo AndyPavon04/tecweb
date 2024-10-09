c67c8c17b925e726c7872994cc2de87bdbc5a113
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-10-08 18:40:06 -0600

    a04: Realizacion ejercicios 7-9 de main.js

--- actividades/a04/src/main.js
+++ actividades/a04/src/main.js
@@ -79,3 +79,72 @@ function ejemplo6(){
         document.write('El número mayor es '+num2);
     }
 }
+
+function ejemplo7(){
+    var nota1,nota2,nota3;
+
+    nota1 = prompt('Ingresa 1ra. nota:', '');
+    nota2 = prompt('Ingresa 2da. nota:', '');
+    nota3 = prompt('Ingresa 3ra. nota:', '');
+
+    
+    nota1 = parseInt(nota1);
+    nota2 = parseInt(nota2);
+    nota3 = parseInt(nota3);
+
+    var pro;
+    pro = (nota1+nota2+nota3)/3;
+
+    if (pro>=7) {
+        document.write('Aprobado');
+    }
+    else {
+        if (pro>=4) {
+        document.write('Regular');
+        }
+        else {
+        document.write('Reprobado');
+        }
+    }
+}
+
+function ejemplo8(){
+    var valor;
+    valor = prompt('Ingresar un valor comprendido entre 1 y 5:', '');
+    //Convertimos a entero
+    valor = parseInt(valor);
+    switch (valor) {
+        case 1: document.write('Uno');
+        break;
+
+        case 2: document.write('Dos');
+        break;
+
+        case 3: document.write('Tres');
+        break;
+
+        case 4: document.write('Cuatro');
+        break;
+
+        case 5: document.write('Cinco');
+        break;
+
+        default:document.write('Debe ingresar un valor comprendido entre 1 y 5.');
+    }
+}
+
+function ejemplo9(){
+    var col;
+    col = prompt('Ingresa el color con que quierar pintar el fondo de la ventana (rojo, verde, azul)' , '');
+    switch (col) {
+        case 'rojo': document.bgColor='#ff0000';
+        break;
+
+        case 'verde': document.bgColor='#00ff00';
+        break;
+
+        case 'azul': document.bgColor='#0000ff';
+        break;
+    }
+}
+
