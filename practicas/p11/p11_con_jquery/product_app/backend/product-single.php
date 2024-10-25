b9fc85d0c2be034e9b556b6fbbe9f8bb6001e553
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-10-25 09:08:55 -0600

    P11: Correccion de caracteres especiales

--- practicas/p11/p11_con_jquery/product_app/backend/product-single.php
+++ practicas/p11/p11_con_jquery/product_app/backend/product-single.php
@@ -9,15 +9,15 @@
         $sql = "SELECT * FROM productos WHERE id = '{$id}'";
 
         if ( $result = $conexion->query($sql) ) {
-            $rows = $result->fetch_all(MYSQLI_ASSOC);
+            $rows = $result->fetch_all(MYSQLI_ASSOC); // OBTIENE TODOS LOS REGISTROS Y LOS GUARDA EN UN ARRAY
 
             if(!is_null($rows)) {
                 foreach($rows as $num => $row) {
                     foreach($row as $key => $value) {
-                        $data[$num][$key] = utf8_encode($value);  // CODIFICA CADA CAMPO EN UTF-8
+                        $data[$num][$key] = $value;  
                     }
                 }
-            }
+            } 
             $result->free();
         } else {
             die('Query Error: '.mysqli_error($conexion));
