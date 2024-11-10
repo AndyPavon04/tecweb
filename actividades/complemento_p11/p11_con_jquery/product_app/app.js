5cb54c945bbf45f816d3e9b19eb5d1327a8f901e
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-11-09 23:25:00 -0600

    complemento p11: Modificacion de app.js e index

--- actividades/complemento_p11/p11_con_jquery/product_app/app.js
+++ actividades/complemento_p11/p11_con_jquery/product_app/app.js
@@ -1,21 +1,19 @@
 // JSON BASE A MOSTRAR EN FORMULARIO
-var baseJSON = {
+/* var baseJSON = {
     "precio": 0.0,
     "unidades": 1,
     "modelo": "XX-000",
     "marca": "NA",
     "detalles": "NA",
-    "imagen": "img/default.png"
-};
+    "imagen": "img/imagen.png"
+}; */
 
-function init() {
-    /**
+/* function init() {
      * Convierte el JSON a string para poder mostrarlo
      * ver: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/JSON
-     */
     var JsonString = JSON.stringify(baseJSON,null,2);
     document.getElementById("description").value = JsonString;
-}
+}*/
 
 $(document).ready(function() {
 
@@ -91,7 +89,9 @@ $(document).ready(function() {
                         template += `
                             <tr productId="${producto.id}">
                                 <td>${producto.id}</td>
-                                <td>${producto.nombre}</td>
+                                <td>
+                                    <a href="#" class="product-item">${producto.nombre}</a>
+                                </td>
                                 <td><ul>${descripcion}</ul></td>
                                 <td>
                                     <button class="product-delete btn btn-danger" data-id="${producto.id}">
@@ -115,15 +115,38 @@ $(document).ready(function() {
                     document.getElementById("products").innerHTML = ""; // Limpiar la tabla de productos
                 }
             },
+            error: function() {
+                alert("Hubo un error al realizar la búsqueda.");
+            }
         });
     });
 
     $('#product-form').submit(function(e) {
         e.preventDefault();
-        var productoJsonString = document.getElementById('description').value;
+
+        // Crear un objeto con las propiedades deseadas
+        var yeison = {
+            id: $('#productId').val(),
+            nombre: $('#form-name').val(),
+            marca: $('#form-brand').val(),
+            modelo: $('#form-model').val(),
+            precio: $('#form-price').val(),
+            detalles: $('#form-story').val(),
+            unidades: $('#form-units').val(),
+            imagen: $('#form-img').val()
+        };
+
+        // Convertir el objeto a JSON
+        var productoJsonString = JSON.stringify(yeison, null, 3);
+
+        // Si necesitas manipular alguna propiedad en `yeison` después
+        yeison['nombre'] = document.getElementById('form-name').value;
+        yeison['id'] = document.getElementById('productId').value;
+
+        // Volver a convertir el objeto actualizado a JSON si es necesario
+        productoJsonString = JSON.stringify(yeison, null, 3);
+
         var finalJSON = JSON.parse(productoJsonString);
-        finalJSON['nombre'] = document.getElementById('name').value;
-        finalJSON['id'] = document.getElementById('productId').value;
         productoJsonString = JSON.stringify(finalJSON, null, 3);
 
         let template_bar = '';
@@ -138,7 +161,7 @@ $(document).ready(function() {
         }
 
         // Validar marca
-        const marcasValidas = ['Epiphone', 'Gibson', 'Taylor', 'Fender', 'Yamaha', 'Oscar Schmidt'];
+        const marcasValidas = ['Epiphone', 'Fender', 'Gibson', 'Oscar Schmidt','Taylor' , 'Yamaha'];
         if (!finalJSON.marca || finalJSON.marca.length == 0) {
             errores.push('Selecciona una marca.');
         }
@@ -166,7 +189,7 @@ $(document).ready(function() {
         if (finalJSON.detalles && finalJSON.detalles.length > 250) {
             errores.push('Los detalles deben tener máximo 250 caracteres.');
         }
-
+        
         // Validar unidades
         if (finalJSON.unidades == null || finalJSON.unidades < 0) {
             errores.push('La cantidad mínima de unidades es 0.');
@@ -213,15 +236,20 @@ $(document).ready(function() {
                     document.getElementById("container").innerHTML = template_bar;
 
                     listadoProductos();
-                    init();
                     edit = false;
                     $('#submit-button').text('Agregar Producto');
-                    $('#name').val('');
+                    $('#form-name').val('');
+                    $('#form-brand').val('');
+                    $('#form-model').val('');
+                    $('#form-price').val('');
+                    $('#form-story').val('');
+                    $('#form-units').val('');
+                    $('#form-img').val('');
+                    $('#productId').val('');
                 }
             });
         }
     });
-
 
     $(document).on('click', '.product-delete', function() {
         if( confirm("De verdad deseas eliminar el Producto") ) {
@@ -247,20 +275,155 @@ $(document).ready(function() {
         }
     });
 
-    $(document).on('click', '.product-item', function() {
-        let id = $(this)[0].parentElement.parentElement.getAttribute('productid');
-        $.post('./backend/product-single.php', {id}, function(response){
+$(document).on('click', '.product-item', function() {
+    let id = $(this)[0].parentElement.parentElement.getAttribute('productId');
+    console.log(id);
+    $.post('./backend/product-single.php', {id}, function(response){
+        try {
             const product = JSON.parse(response);
-            $('#name').val(product[0].nombre);
-            let productWithoutNameAndId = {...product[0]};
-            delete productWithoutNameAndId.nombre;
-            delete productWithoutNameAndId.id;
-            delete productWithoutNameAndId.eliminado;
+            console.log(response);
 
-            
-            edit = true;
-            $('#description').val(JSON.stringify(productWithoutNameAndId, null, 4));
-            $('#submit-button').text('Editar Producto');
-        })
+            if (product && product[0]) { // Check if product and product[0] exist
+                $('#productId').val(id);
+                $('#form-name').val(product[0].nombre);
+                $('#form-brand').val(product[0].marca);
+                $('#form-model').val(product[0].modelo);
+                $('#form-price').val(product[0].precio);
+                $('#form-story').val(product[0].detalles);
+                $('#form-units').val(product[0].unidades);
+                $('#form-img').val(product[0].imagen);
+
+                edit = true;
+                $('#submit-button').text('Editar Producto');
+            } else {
+                console.error("Product not found or invalid response.");
+                // Handle the case where the product is not found or the response is invalid.
+                // You could display an error message to the user.
+            }
+        } catch (error) {
+            console.error("Error parsing JSON response:", error);
+            // Handle the case where the response is not valid JSON.
+        }
+    })
+});
+
+    $('#form-name').keyup(function(e) {
+        e.preventDefault();
+
+        var name = $('#form-name').val();
+
+        $.ajax({
+            url: './backend/product-singleByName.php',
+            type: 'GET',
+            data: { name: name },
+            success: function(response) {
+                let productos = response;
+                if(productos.length > 2) {
+                    let template_bar = '';
+                    template_bar += `<li>Ya existe un producto con ese nombre</li>`;
+                    document.getElementById("product-result").className = "card my-4 d-block";
+                    document.getElementById("container").innerHTML = template_bar;
+                }
+                else{
+                    let template_bar = '';
+                    template_bar += `<li>El nombre es valido</li>`;
+                    document.getElementById("product-result").className = "card my-4 d-block";
+                    document.getElementById("container").innerHTML = template_bar;
+                }
+            }
+
+        });
     });
 });
+
+function validarNombre(){
+    var nombre = document.getElementById("form-name");
+    if(nombre.value == ''){
+        let template_bar = '';
+        template_bar += `<li>El campo nombre es obligatorio</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+    else if (nombre.value.length > 100){
+        let template_bar = '';
+        template_bar += `<li>El nombre debe tener como maximo 100 caracteres</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+
+    }
+}
+
+function validarMarca(){
+    var marca = document.getElementById("form-brand");
+    if (marca.value == ""){
+        let template_bar = '';
+        template_bar += `<li>El campo marca es obligatorio</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+}
+
+function validarModelo(){
+    var modelo = document.getElementById("form-model");
+    if (modelo.value == ''){
+        let template_bar = '';
+        template_bar += `<li>Ingresa un modelo</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+}
+
+function validarPrecio(){
+    var precio = document.getElementById("form-price");
+    if (precio.value == ''){
+        let template_bar = '';
+        template_bar += `<li>Ingresa el precio</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+    else if (precio.value <= 99.99){
+        let template_bar = '';
+        template_bar += `<li>El precio debe ser mayor a $99.99</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+}
+
+function validarDetalles(){
+    var detalles = document.getElementById("form-details");
+    if(detalles.value > 250)
+    {
+        let template_bar = '';
+        template_bar += `<li>Los detalles deben tener máximo 250 caracteres</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+}
+
+function validarUnidades(){
+    var unidades = document.getElementById("form-units");
+    if (unidades.value == ''){
+        let template_bar = '';
+        template_bar += `<li>El campo unidades es obligatorio</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+    else if (unidades.value < 0){
+        let template_bar = '';
+        template_bar += `<li>Cantidad mínima de unidades es 0</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+    }
+}
+
+function validarImagen(){
+    var imagen = document.getElementById("form-img");
+    if (imagen.value == ''){
+        let template_bar = '';
+        template_bar += `<li>Se asignó una imagen por defecto</li>`;
+        document.getElementById("product-result").className = "card my-4 d-block";
+        document.getElementById("container").innerHTML = template_bar;
+
+        imagen.value = "img/default.png";
+    }
+}
