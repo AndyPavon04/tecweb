61d9bb454f72d3f063be04e17fb36531f427cb83
AndyPavon04 <andy.perezp@alumno.buap.mx>
2024-11-10 18:02:12 -0600

    P12: Archivos creados en myapi

--- practicas/p12/product_app/backend/myapi/DataBase.php
+++ practicas/p12/product_app/backend/myapi/DataBase.php
@@ -1,26 +1,13 @@
 <?php
-namespace TECWEB\MYAPI;
+    namespace TECWEB\MYAPI;
 
-abstract class DataBase {
-    protected $conexion;
+    abstract class DataBase {
+        protected $conexion;
+        protected $data;
 
-    public function __construct($db, $user, $pass) {
-        $this->conexion = @mysqli_connect(
-            'localhost',
-            $user,
-            $pass,
-            $db
-        );
-    
-        /**
-         * NOTA: si la conexión falló $conexion contendrá false
-         **/
-        if(!$this->conexion) {
-            die('¡Base de datos NO conextada!');
+        public function __construct($db, $user, $pass) {
+            $this->conexion = new \mysqli('localhost', 'root', '12345', 'marketzone');
+            $this->data = array();
         }
-        /*else {
-            echo 'Base de datos encontrada';
-        }*/
     }
-}
 ?>
